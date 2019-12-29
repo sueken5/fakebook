116dec7e3f803b0ca11f61a4bc3d6390211074fd
sueken5 <sueken0117@gmail.com>
2019-12-29 13:59:36 +0900

    add profile

--- Profile.tsx
+++ Profile.tsx
@@ -1,9 +1,11 @@
 import {StyleSheet, Text, View} from "react-native";
 import * as React from "react";
+import {Panel} from "./components/Profile/Panel";
 
 export const Profile = () => (
     <View style={[styles.scene, { backgroundColor: '#fff' }]} >
         <Text>Hello Fakebook Profile</Text>
+        <Panel />
     </View>
 );
 
---
+++ components/Profile/Panel.tsx
@@ -0,0 +1,25 @@
+import {Image, StyleSheet, Text, View} from "react-native";
+import * as React from "react";
+import {Icons} from "./Icons";
+
+export const Panel = () => (
+    <View style={{ backgroundColor: '#fff'}}>
+        <View style={{height: 300}}>
+            <Image style={{height: 200, position: "relative"}} source={require('../../assets/back_sakura.jpg')} />
+            <View style={{backgroundColor: "white", borderRadius: 100, padding: 3, position:"absolute", top: 120, left: 120}}>
+                <Image style={{ width: 150, height: 150, borderRadius: 100 }} source={require('../../assets/kawaii_neco.jpg')} />
+            </View>
+        </View>
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+            <Text style={{fontSize: 50}}>猫です</Text>
+            <Text>nyaaaaaaaaaaan</Text>
+        </View>
+        <Icons />
+    </View>
+);
+
+const styles = StyleSheet.create({
+    scene: {
+        flex: 1,
+    },
+});
