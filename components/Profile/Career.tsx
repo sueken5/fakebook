da79f21a4725d34e675eb9a01c719fa8beeecaa6
sueken5 <sueken0117@gmail.com>
2019-12-29 14:29:21 +0900

    add padding

--- components/Profile/Career.tsx
+++ components/Profile/Career.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 
 export const Career = (props: Props) => (
-    <View style={{flexDirection:"row"}}>
+    <View style={{flexDirection:"row", padding: 5}}>
         <Image style={{height: 20, width: 20}} source={props.source}/>
         <View style={{paddingHorizontal: 5}}>
             <Text style={{fontSize: 20}}>{props.description}</Text>
